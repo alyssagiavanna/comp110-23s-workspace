657157a2cff2d4672fdbf8c05741ed37b485d267
Alyssa Giangrasso <alyssa.giangrasso@gmail.com>
2023-02-28 14:58:35 -0500

    backup

--- exercises/ex03_wordle.py
+++ exercises/ex03_wordle.py
@@ -10,39 +10,45 @@ def contains_char(word: str, one_letter: str) -> bool:
     assert len(one_letter) == 1
     idx_1: int = 0
 
-    while idx_1 < len(word):  
+    while idx_1 < len(word):
+    #making sure the index doesnt go outside of the word's highest index  
         if word[idx_1] == one_letter:
             return True
+        #checking if letter is at each index
         else: 
             idx_1 = idx_1 + 1
+        #continues looking through each of the word's indexes
     return False
 
-def emojified(guess: str, secret: str) -> str:
-    """Testing for yellow or white box codification"""
-    assert len(guess) == len(secret)
+def emojified(guess_word: str, secret_word: str) -> str:
+    """Testing for green or yellow or white box codification"""
+    assert len(guess_word) == len(secret_word)
     idx_2: int = 0
     guess_box: str = " "
-    
-    while idx_2 < len(guess):
+
+    while idx_2 < len(guess_word):
     #while the index is less than the length of guess word    
-        if (guess[idx_2] == secret[idx_2]):
-        #if the guess word contains a letter in from the secret word and they are in the same index return green emoji box
+        if (guess_word[idx_2] == secret_word[idx_2]):
+        #if the letter in the guess word's index and the secret word's index are the same return green emoji box
             guess_box = guess_box + GREEN_BOX
-        elif (contains_char(secret,guess[idx_2]) is True):
-        #if the guess word contains a letter in from the secret word and they are not in the same index return yellow emoji box    
+        elif (contains_char(secret_word,guess_word[idx_2]) is True):
+        #if the guess word contains a letter in the secret word and they are not in the same index return yellow emoji box    
             guess_box = guess_box + YELLOW_BOX
         else:
         #if the guess word contains no letters in the secret word return white emoji box
             guess_box = guess_box + WHITE_BOX
         idx_2 = idx_2 + 1
+        #continues looking through for each index
     return guess_box
 
 def input_guess(word_length: int) -> str:
-    """vgjasvhcdsakfBKa"""
+    """Making sure the word guessed matches the length of letters in the inputed word"""
     first_guess: str = input(f"Enter a {word_length} character word: ")
 
     while len(first_guess) != word_length:
+    #while the length of the guess word does not equal the length of the inputed word
         first_guess = input(f"That wasn't {word_length} chars! Try again: ")
+        #tells user it is not the correct amount of characters in the word and asks to submit a new word
     return first_guess
 
 def main() -> None:
@@ -52,18 +58,24 @@ def main() -> None:
     try_again: bool = False
 
     while (trial <= 6) and (try_again is False):
+    #while the player is within the max 6 tries and has not guessed the secret word, game continues
         print(f"=== Turn {trial}/6 ===")
-        #tells player how many trys left
+        #tells player how many tries left
         player_guess: str = input_guess(len(secret))
-        print(emojified(player_guess,secret))
+        #prompts player to guess a word
+        print(emojified(player_guess, secret))
+        #shows player the enoji hint to the secret word's characters
         if player_guess == secret:
             print(f"You won in {trial}/6 turns!")
             try_again = True
+        #player guesses word and game is over
         else:
             trial = trial + 1
+        #player incorrectly guesses word and has one less try than before
     
     if try_again == False:
         print("X/6 - Sorry, try again tomorrow!")
+    #player has run out of tries and loses the game
 
 if __name__ == "__main__":
     main()
---
+++ lessons/imports.py
@@ -0,0 +1,13 @@
+"""Practice importing from other modules"""
+
+from lessons import my_functions
+
+if __name__ == "__main__":
+    print("Howdy!")
+
+print(my_functions.addition(1,2))
+print(my_functions.my_variable)
+
+from lessons.my_functions import addition
+
+print(addition(1,2))
